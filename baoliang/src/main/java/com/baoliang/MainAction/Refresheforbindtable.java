7f02a27b0b8f88e58e014051550f254b98da90c4
GuanYunChang <zhubaoliang@aliyun.com>
2018-01-20 20:38:01 +0800

    fix application

---
+++ baoliang/src/main/java/com/baoliang/MainAction/Refresheforbindtable.java
@@ -0,0 +1,123 @@
+/**
+ *This is baoliang project
+ *@author:baoliang
+ **/
+package com.baoliang.MainAction;
+
+import java.util.Calendar;
+import java.util.List;
+
+import org.json.JSONException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.baoliang.Model.Application;
+import com.baoliang.Model.ApplicationDaoImp;
+import com.baoliang.Tools.jsontools;
+import com.opensymphony.xwork2.ActionSupport;
+
+public class Refresheforbindtable  extends ActionSupport {
+
+	private int pagesum;
+	public int getPagesum() {
+		return pagesum;
+	}
+
+
+
+	public void setPagesum(int pagesum) {
+		this.pagesum = pagesum;
+	}
+
+
+
+	private int indexup;
+	public int getIndexup() {
+		return indexup;
+	}
+
+
+
+	public void setIndexup(int indexup) {
+		this.indexup = indexup;
+	}
+
+
+
+	public int getIndexdown() {
+		return indexdown;
+	}
+
+
+
+	public void setIndexdown(int indexdown) {
+		this.indexdown = indexdown;
+	}
+
+
+
+	public String getDataup() {
+		return dataup;
+	}
+
+
+
+	public void setDataup(String dataup) {
+		this.dataup = dataup;
+	}
+
+
+
+	public String getDatadown() {
+		return datadown;
+	}
+
+
+
+	public void setDatadown(String datadown) {
+		this.datadown = datadown;
+	}
+
+
+
+	private int indexdown;
+	private String dataup;
+	private String datadown;
+	public String getFlag() {
+		return flag;
+	}
+
+
+
+	public void setFlag(String flag) {
+		this.flag = flag;
+	}
+
+
+
+	private String flag="false";
+	
+	
+	public String refreshtableup() throws JSONException, IllegalArgumentException, IllegalAccessException 
+	{
+		ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{ "applicationContext.xml"});
+		ApplicationDaoImp apc= (ApplicationDaoImp) context.getBean("ApplicationDaoImp");
+		List objlist=apc.findAll("1");
+		Integer sum=objlist.size();
+		Integer start=(getPagesum()-1)*getIndexup()+1;
+		if(start<1)
+			start=1;
+		Integer end=start+getPagesum();
+		if(end>sum)
+		{
+			end=sum;
+			
+		}
+			objlist=objlist.subList(start, end);
+			System.out.println("start:"+start+",end:"+end);
+		setDataup(jsontools.tojsonForNoArray(objlist,Application.class ));
+		return SUCCESS;
+		
+		
+	}
+}
--- baoliang/src/main/webapp/Main/js/Bindapanddriver.js
+++ baoliang/src/main/webapp/Main/js/Bindapanddriver.js
@@ -1,5 +1,128 @@
-function poptest()
+/*function poptest()
 {
 	var popup_url="http://"+window.location.hostname+":"+window.location.port+"/baoliang/Popwindow/poptest.jsp";
 	window.open(popup_url,'newwindow','width='+500+',height='+350+',top='+100+',left='+300+',toolbar=no,menubar=no,scrollbars=yes,resizable=no,location=no,status=no');
+}*/
+
+var tableup=1;
+var tabledown=0;
+var setv;
+$(function(){
+	
+	
+	refreshup();
+	setv=setInterval("refreshup()",3000);
+});
+function refreshup()
+{
+	
+	$.ajax({
+		type:"post",
+		url:"refreshtableup",
+		data:"indexup="+tableup+"&pagesum=2",
+		dataType:"json",
+		async:true,
+		timeout:5000,
+		success:function(data)
+		{
+			
+			 
+			 var str='<tr>'
+				 +'<td>订单编号</td>'
+				 +'<td>订单人</td>'
+				 +'<td>订单人电话</td>'
+				 +'<td>货物</td>'
+				 +'<td>始发地</td>'
+				 +'<td>目的地</td>'
+				 +'<td>司机编号</td>'
+				 +'<td>车牌号</td>'
+				 +'<td>重量</td>'
+				 +'</tr>';
+			 
+			var mydata = eval('(' + data.dataup + ')');
+			if(data.flag=="true")
+				{
+					
+				}else
+					{
+					
+					document.getElementById('uppage').innerHTML="第"+tableup+"页";
+					}
+			var i;
+			for(i=0;i<mydata.length;i++)
+				{
+			 str=str+'<tr>'
+			 +'<td>'+mydata[i].acnum+'</td>'
+			 +'<td>'+mydata[i].boss+'</td>'
+			 +'<td>'+mydata[i].phone+'</td>'
+			 +'<td>'+mydata[i].goods+'</td>'
+			 +'<td>'+mydata[i].start+'</td>'
+			 +'<td>'+mydata[i].destination+'</td>'
+			 +'<td>'+mydata[i].drivernum+'</td>'
+			 +'<td>'+mydata[i].car+'</td>'
+			 +'<td>'+mydata[i].weight+'</td>'
+			 +'</tr>';
+			 }
+			document.getElementById('apdata').innerHTML=str;
+			
+		},
+		
+	
+		error:function(XMLHttpRequest, textStatus, errorThrown){
+			alert("网络请求失败");
+			clearInterval(setv);
+		}
+		
+	});
+	
+}
+function next()
+{
+	tableup++;
+	console.log(tableup);
+	clearInterval(setv);
+	setv=setInterval("refreshup()",3000);
+	//refreshup();
+}
+function preview()
+{
+	if(tabledown>1)
+		{
+
+			$.ajax({
+				type:"post",
+				url:"addpreview",
+				data:"index="+tabledown,
+				dataType:"json",
+				success:function(data)
+				{
+					tabledown--;
+					document.getElementById('downpage').innerHTML="第"+tabledown+"页";
+					var str='<tr>'
+						+'<td>司机编号</td>'
+						+'<td>姓名</td>'
+						+'<td>电话</td>'
+						+'<td>车号</td>'
+						+'<td>装载量</td>'
+						+'<td>选择</td>'
+					+'</tr>';
+				
+				},
+				error:function(){
+					
+					alert("获取数据失败");
+				}
+				
+			});
+		
+		
+		}
+}
+function addtableup()
+{
+	
+}
+function addtabledown()
+{
+	
 }
